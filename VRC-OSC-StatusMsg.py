71ec1cc85365509889b3979fa09b9430d3e95c4a
August <147424439+August-WW@users.noreply.github.com>
2026-03-13 08:42:08 -0400

    Code format edit

--- VRC-OSC-StatusMsg.py
+++ VRC-OSC-StatusMsg.py
@@ -3,12 +3,14 @@ import time
 
 client = SimpleUDPClient("127.0.0.1", 9000)
 print("Running... You may now return to VRChat")
-print("\n\nOpen VS Code (or other editor of your choice) to change your message\n(must rerun the .py file after changes are made)")
+print("\n\nOpen VS Code (or other editor of your choice) to change your message")
+print("\n(must rerun the .py file after changes are made)")
 print("\n\nNot working? Make sure you have OSC enabled")
 print("\n\ngithub.com/August-WW/VRC-OSC-Status")
 print("\n\n[august wolf]")
 while True:
     client.send_message("/chatbox/input", ["(your message)", True, False])
     time.sleep(5)   # Very important - could cause rate limits and other issues without it
+
 
 # [august wolf]
